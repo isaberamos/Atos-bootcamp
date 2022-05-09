a922ec5bc859d7b5d977af44d2afc232fffbc310
Isabella Ramos <90662086+isaberamos@users.noreply.github.com>
2022-05-09 09:58:59 -0300

    Update SQL_Dinossauro1.sql

--- src/SQL - Dinossauro/SQL_Dinossauro1.sql
+++ src/SQL - Dinossauro/SQL_Dinossauro1.sql
@@ -159,3 +159,5 @@ AND dinossauro.id_era = era.id
 AND grupo.nome = 'Anquilossauros'
 AND dinossauro.ano_descoberta between 1900 AND 1999
 ORDER BY dinossauro.ano_descoberta;
+
+select * from dinossauros
